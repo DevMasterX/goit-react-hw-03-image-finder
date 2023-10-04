766f022de63ffc0b2e1947fa6df6f4d18760df0b
DevMasterX <sokol080288@gmail.com>
2023-10-04 23:47:36 +0300

    changes

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class SearchBar extends Component {
+    state = {
+        searchName: '',
+        inputValue: '',
+    } 
+    render() { 
+        return ();
+    }
+}
+ 
+export default SearchBar;
