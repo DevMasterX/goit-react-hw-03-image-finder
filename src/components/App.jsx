f766f1610b16bd04e2d193e2ffcfad4ecc543388
DevMasterX <sokol080288@gmail.com>
2023-10-06 00:33:36 +0300

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import { Component } from 'react';
 
 
-export default class App extends Component {
-  state = {};
+class App extends Component {
+  state = {
+     searchName: '',
+    images: [],
+    currentPage: 1,
+    error: null,
+    isLoading: false,
+    totalPages: 0,
+    visible: false,
+  };
   render() {
     return ();
   }
 }
+
+
+export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,13 +1,45 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 class SearchBar extends Component {
-    state = {
-        searchName: '',
-        inputValue: '',
-    } 
-    render() { 
-        return ();
-    }
+  state = {
+    searchName: '',
+    inputValue: '',
+  };
+
+  handleChange = event => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const searchQuery = event.target.elements.searchName.value.trim();
+    this.props.onSubmit(searchQuery);
+    event.target.reset();
+  };
+
+  render() {
+    return (
+      <header class="searchbar">
+        <form class="form" onSubmit={this.handleSubmit}>
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
+
+          <input
+            name="searchName"
+            class="input"
+            type="text"
+            id="search"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
 }
- 
+
 export default SearchBar;
