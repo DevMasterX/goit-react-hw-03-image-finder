3a00ecf015575748fc2c5a2f9bcb531047f19a23
DevMasterX <sokol080288@gmail.com>
2023-10-05 22:59:50 +0300

    changes

--- src/services/PixabayApi.jsx
+++ src/services/PixabayApi.jsx
@@ -3,3 +3,15 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '39861298-38e1e10cd9c08fce6ef5c3957';
 export const perPage = 12;
+
+export const getImages = async (query, page) => {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response.data;
+};
+
+export const normalizedImages = imageArray =>
+  imageArray.map(({ id, tags, webformatURL, largeImageURL }) => {
+    return { id, tags, webformatURL, largeImageURL };
+  });
